cc7ea148964edda5f110ad2874b5be7ef720de3d
gcalzada <gustavo_0073@hotmail.com>
2016-01-24 14:47:43 -0600

    P1

--- Plot1.R
+++ Plot1.R
@@ -6,4 +6,5 @@ data$Global_active_power<-as.numeric(data$Global_active_power)
 
 png("plot1.png", width=480, height=480)
 hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
 dev.off()
